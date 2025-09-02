23e48db5f03135ab2ffd8b781c044c4093b34ac5
jiiean <jaz28@duke.edu>
2025-09-02 17:06:29 +0800

    hashrouting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import LandingPage from "./LandingPage";
 import ImpactPage from "./ImpactPage";
